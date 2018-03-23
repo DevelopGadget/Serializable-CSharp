6b0e603f84abc22645f392e1ee29b0195e06a32f
FernanPro18 <fernandoaraujo081@gmail.com>
2018-03-23 16:40:11 -0500

    Estructura

---
+++ Serializable/Controller/EquipoController.cs
@@ -0,0 +1,45 @@
+﻿
+using Serializable.Model;
+using System.Collections.Generic;
+
+namespace Serializable.Controller
+{
+    class EquipoController
+    {
+        public List<Equipo> Equipos { get; set; }
+
+        public EquipoController()
+        {
+            Equipos = new List<Equipo>();
+        }
+
+        public void Create(Equipo Equipo)
+        {
+            Equipos.Add(Equipo);
+        }
+
+        public List<Equipo> Read(string Filter)
+        {
+            List<Equipo> Read = new List<Equipo>();
+            foreach (Equipo Equipo in Equipos)
+            {
+                if (Equipo.Nombre.Contains(Filter))
+                {
+                    Read.Add(Equipo);
+                }
+            }
+            return Read;
+        }
+
+        public void Update(int Index, Equipo Equipo)
+        {
+            Equipos.Insert(Index, Equipo);
+        }
+
+        public void Delete(int Index)
+        {
+            Equipos.RemoveAt(Index);
+        }
+
+    }
+}
---
+++ Serializable/Model/Equipo.cs
@@ -0,0 +1,22 @@
+﻿
+using System;
+
+namespace Serializable.Model
+{
+    [Serializable]
+    class Equipo
+    {
+        public string Nombre { get; set; }
+        public string Estadio { get; set; }
+        public Uri uEstadio { get; set; }
+        public Uri uEscudo { get; set; }
+
+        public Equipo(string sNombre, string sEstadio, Uri uEstadio, Uri uEscudo)
+        {
+            this.Nombre = sNombre;
+            this.Estadio = sEstadio;
+            this.uEstadio = uEstadio;
+            this.uEscudo = uEscudo;
+        }
+    }
+}
